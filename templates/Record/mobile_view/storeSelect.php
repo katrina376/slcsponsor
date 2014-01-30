a602d637a6e4e06bdc00a100eb6e769c3e0d543a
katrina376 <b00209023@ntu.edu.tw>
2014-01-31 05:15:20 +0800

    correction!

--- templates/Record/mobile_view/storeSelect.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$pages = array('info' => 'Info', 'record' => 'Past Records', 'comment' => 'Comments');
-	$count = 0;
-	$storeID = $_GET['id'];
-	/*$title = */                             // get from database
-?>
